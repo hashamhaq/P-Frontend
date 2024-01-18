6608498d3405975ffd596a9acbe4eb1d3f31183e
hashamhaq <155137509+hashamhaq@users.noreply.github.com>
2024-01-18 15:07:38 +0500

    Update server.js
    2nd

--- server.js
+++ server.js
@@ -2,6 +2,6 @@ const express = require('express');
 const app = express();
 const port = 5002;
 
-app.get('/', (req, res) => res.send('Frontend file response first change 5002'));
+app.get('/', (req, res) => res.send('Frontend file 2nd change 5002'));
 
 app.listen(port, () => console.log(`App 1 listening on port ${port}`));
