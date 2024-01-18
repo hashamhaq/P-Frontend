1a5f2fc5a77cbcae34a771fd85b1a10312ced07d
hashamhaq <155137509+hashamhaq@users.noreply.github.com>
2024-01-18 15:32:09 +0500

    Update server.js
    3

--- server.js
+++ server.js
@@ -2,6 +2,6 @@ const express = require('express');
 const app = express();
 const port = 5002;
 
-app.get('/', (req, res) => res.send('Frontend file 2nd change 5002'));
+app.get('/', (req, res) => res.send('Frontend file 3rd change through git 5002'));
 
 app.listen(port, () => console.log(`App 1 listening on port ${port}`));
